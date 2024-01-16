ddc5f78bb980dd7a8ba753a761e0398f20eb40ca
zaingillani <abdullahgillaniv204@gmail.com>
2024-01-16 08:51:22 +0500

    .

---
+++ marrage_hall/Pages/blog.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace marrage_hall.Pages
+{
+    public class blogModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ marrage_hall/Pages/team.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace marrage_hall.Pages
+{
+    public class teamModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
